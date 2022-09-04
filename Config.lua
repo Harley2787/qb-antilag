fc5afb5d6afce3a8ce830c683ea8bfd5d885b5fd
Harley <83428883+Harley2787@users.noreply.github.com>
2022-09-04 18:44:10 +0100

    Added
    Added V 1.1.5

---
+++ Config.lua
@@ -0,0 +1,15 @@
+	--[[
+		Antilag & 2-Step Script
+			By Silence & Samuel_
+		But Configurations...
+	]]
+
+Config = {
+		-- 21 (Left Shift by default)
+	twoStepControl = 21,
+		
+		-- Add Vehicles to this list to allow the 2step/Anti-lag engagement -- Put more cars into one line to save vertical space
+	TwoStepCars = {
+	"ffrs",
+	}
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+game 'gta5'
+fx_version 'cerulean'
+
+author 'HarleyB123'
+description 'Updated By HarleyB123 | [QBCORE]'
+discord 'https://discord.gg/fNW2rTGEBg'
+version '1.1.5'
+
+client_scripts {
+	'cl-2Step.lua'
+}
+
+shared_scripts {
+	'Config.lua'
+}
+
+server_scripts {
+	'sv-2Step.lua'
+}
+
+dependencies {
+	'qb-core'
+}
